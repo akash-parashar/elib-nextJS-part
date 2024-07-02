0ea8cf16feb36cf54b26454b29dac04fb3d518a6
akash-parashar <aparashar508@gmail.com>
2024-07-03 01:38:32 +0530

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hey</h1>
+    <>
+    <Banner/>
+    </>
   );
 }
