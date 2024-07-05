71f5b8167658c246652a79f8abc5d9d5639fb5a6
akash-parashar <aparashar508@gmail.com>
2024-07-06 00:45:06 +0530

    create book card

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,17 @@
+import Banner from '@/app/(home)/components/Banner';
+import Image from 'next/image';
+import BookList from './components/BookList';
+import { Suspense } from 'react';
+import Loading from '@/components/Loading';
+// export const dynamic = 'force-dynamic';
 
-import Image from "next/image";
-import Banner from "./components/Banner";
-
-export default function Home() {
-  return (
-    <>
-    <Banner/>
-    </>
-  );
+export default async function Home() {
+    return (
+        <>
+            <Banner />
+            <Suspense fallback={<Loading />}>
+                <BookList />
+            </Suspense>
+        </>
+    );
 }
